a3fb4635ff8f252e5a0b28cbdb4623eea0f697e5
Evanson Wainaina <[evanson1053@gmail.com]>
2026-07-19 16:56:23 +0300

    added netfly plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), 
+    reactRouter(),
+    netlifyReactRouter()
+  ],
   server: {
     allowedHosts: ["2ccf-102-206-97-58.ngrok-free.app"]
   }
